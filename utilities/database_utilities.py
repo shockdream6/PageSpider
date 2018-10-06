bf9dbad954f494f0c1b437b5b942e0f6d8fc5fa1
shockdream6 <36081509+shockdream6@users.noreply.github.com>
2018-10-06 10:54:36 +0530

    First and final commit

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,33 @@
-def create_database(database_path: str):
-    #TODO: generate the database
-    pass
+import sqlite3 as lite
 
-def save_words_to_database(database_path:str, words_list: list):
-    #TODO: save the words to the database
-    pass
-#random comment today
+
+def create_database(database_path: str):
+    # generate the database and drops if already created
+    connection = lite.connect(database_path)
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("drop table if exists words")
+        ddl = "CREATE TABLE words(word TEXT PRIMARY KEY NOT NULL, usage_count INT DEFAULT 1 NOT NULL); "
+        cursor.execute(ddl)
+        ddl = "CREATE UNIQUE INDEX words_word_uindex ON words (word)"
+        cursor.execute(ddl)
+        # connection.close()
+
+
+def save_words_to_database(database_path: str, words_list: list):
+    # save the words to the database
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            # check to see if world is already in there
+            sql = "SELECT count(word) FROM words WHERE word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "UPDATE words SET usage_count = usage_count + 1 WHERE word = '" + word + "'"
+            else:
+                sql = "INSERT INTO words(word) VALUES ('" + word + "')"
+            cur.execute(sql)
+        print("Database save complete!")
+    conn.close()
